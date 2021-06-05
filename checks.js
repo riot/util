fc83fd7a7721a989796a6ac9dbf0f577440315b9
gianlucaguarini <gianluca.guarini@gmail.com>
2021-06-05 21:37:16 +0200

    closes https://github.com/riot/ssr/issues/23

--- checks.js
+++ checks.js
@@ -25,7 +25,7 @@ export function isSvg(el) {
  * @returns {boolean} true if it's a <template>
  */
 export function isTemplate(el) {
-  return el.tagName === 'TEMPLATE'
+  return el.tagName.toLowerCase() === 'template'
 }
 
 /**

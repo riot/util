f37e26b710405665ff4dac46a0f3e69033a3d9f0
Gianluca Guarini <gianluca.guarini@gmail.com>
2025-02-28 21:19:14 +0100

    added: Ref expression type

--- expression-types.js
+++ expression-types.js
@@ -2,10 +2,12 @@ export const ATTRIBUTE = 0
 export const EVENT = 1
 export const TEXT = 2
 export const VALUE = 3
+export const REF = 4
 
 export default {
   ATTRIBUTE,
   EVENT,
   TEXT,
   VALUE,
+  REF,
 }
